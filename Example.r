af69de88a1d6b349ad4e740aa4ad61102a875e05
parth340 <64318915+parth340@users.noreply.github.com>
2021-08-28 15:21:10 +0530

    Add files via upload

---
+++ Example.r
@@ -0,0 +1,36 @@
+
+1+1
+2+3*4
+3^2
+exp(1)
+sqrt(10)
+pi
+x<-10
+y<-15
+z<-5
+x*y*z
+Ex <-c(5,0,0,12)
+x <- c(2,0,0,4)
+y<-c(1,9,9,9)
+x+y
+x*4
+sqrt(x)
+Ex[4]
+x<- c(2,0,0,4)
+x[1]            #Select the first element, equivalent to x[x(1)] 
+x[-1]           #Exclude the first element
+x[1]<-3
+x[-1] =5
+y<9             #Compares each element, returns result as vector
+y[4] =1
+y<9
+
+y[y<9] = 2      #Edits 
+y
+
+df <- data.frame(x=1:3, y=c("a","b","c"))
+df[1,1]
+df[1,c(1,2)]
+df[c(1,3),2]
+df[c(1,3),1]
+df[c(1,3),c(1,2)]
